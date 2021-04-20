1e7f4ba5fe57ffa83930d0d40ab7af6c7d714637
jasperwreed <jmixon.reed@gmail.com>
2021-04-20 14:26:03 -0700

    deleted

--- minimum_depth_binary_tree/minDepth.js
+++ minimum_depth_binary_tree/minDepth.js
@@ -1,12 +1,5 @@
 const minDepth = root => {
     if (!root) return null
     
-    let right = minDepth(root.right)
-    let left = minDepth(root.left)
     
-    if (root.right && root.left) {
-        return Math.min(left, right) + 1
-    } else {
-        return Math.max(left, right) + 1
-    }
 }
