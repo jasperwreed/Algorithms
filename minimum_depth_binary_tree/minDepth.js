5acad7e032eb0b2b8648466bd2a04bb399809836
jasperwreed <jmixon.reed@gmail.com>
2021-04-20 14:33:05 -0700

    done

--- minimum_depth_binary_tree/minDepth.js
+++ minimum_depth_binary_tree/minDepth.js
@@ -1,18 +1,4 @@
 var minDepth = function(root) {
     if (!root) return null
-    let depth = 1
-    let arr = []
     
-    arr.push(root)
-    
-    while (arr.length) {
-        let constantLength = arr.length
-        for (let i = 0; i < constantLength; i++) {
-            let current = arr.shift()
-            if (!current.left && !current.right) return depth
-            if (current.left) arr.push(current.left)
-            if (current.right) arr.push(current.right)
-        }
-        depth++
-    }
 };
