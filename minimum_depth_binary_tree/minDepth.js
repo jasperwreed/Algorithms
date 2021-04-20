19091f0ec9461f5387b85450522a5b573573e25b
Jasper Reed <jasper.reed@flatironschool.com>
2021-04-20 15:00:21 -0700

    done done

--- minimum_depth_binary_tree/minDepth.js
+++ minimum_depth_binary_tree/minDepth.js
@@ -1,42 +1,45 @@
-var minDepth = function(root) {
-    if (!root) return null
-    
-    arr.push(root)
-    
-    while (arr.length) {
-        let constantLength = arr.length
-        for (let i = 0; i < constantLength; i++) {
-            let current = arr.shift()
-            if (!current.left && !current.right) return depth
-            if (current.left) arr.push(current.left)
-            if (current.right) arr.push(current.right)
-        }
-        depth++
+var minDepth = function (root) {
+  if (!root) return null;
+
+  arr.push(root);
+
+  while (arr.length) {
+    let constantLength = arr.length;
+    for (let i = 0; i < constantLength; i++) {
+      let current = arr.shift();
+      if (!current.left && !current.right) return depth;
+      if (current.left) arr.push(current.left);
+      if (current.right) arr.push(current.right);
     }
+    depth++;
+  }
 };
 
-const minDepth = root => {
-    if (!root) return null
-    
-    let right = minDepth(root.right)
-    let left = minDepth(root.left)
-    
-    return (root.right && root.left ? Math.min(left, right) : Math.max(left,right)) + 1
-}
+const minDepth = (root) => {
+  if (!root) return null;
 
-var minDepth = function(root) {
-    if (!root) return null
-    let depth = 1
-    let queue = [root]
-    
-    while (queue.length) {
-        let constantLength = queue.length
-        for (let i = 0; i < constantLength; i++) {
-            let current = queue.shift()
-            if (!current.left && !current.right) return depth
-            if (current.left) queue.push(current.left)
-            if (current.right) queue.push(current.right)
-        }
-        depth++
+  let right = minDepth(root.right);
+  let left = minDepth(root.left);
+
+  return (
+    (root.right && root.left ? Math.min(left, right) : Math.max(left, right)) +
+    1
+  );
+};
+
+var minDepth = function (root) {
+  if (!root) return null;
+  let depth = 1;
+  let queue = [root];
+
+  while (queue.length) {
+    let constantLength = queue.length;
+    for (let i = 0; i < constantLength; i++) {
+      let current = queue.shift();
+      if (!current.left && !current.right) return depth;
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
     }
+    depth++;
+  }
 };
