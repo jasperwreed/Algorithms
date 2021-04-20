91a2bd0f7e3c05c847fdc052f51d9848a4271ef6
Jasper Reed <jasper.reed@flatironschool.com>
2021-04-20 14:39:31 -0700

    done

--- minimum_depth_binary_tree/minDepth.js
+++ minimum_depth_binary_tree/minDepth.js
@@ -16,3 +16,20 @@ var minDepth = function(root) {
         depth++
     }
 };
+
+var minDepth = function(root) {
+    if (!root) return null
+    let depth = 1
+    let queue = [root]
+    
+    while (queue.length) {
+        let constantLength = queue.length
+        for (let i = 0; i < constantLength; i++) {
+            let current = queue.shift()
+            if (!current.left && !current.right) return depth
+            if (current.left) queue.push(current.left)
+            if (current.right) queue.push(current.right)
+        }
+        depth++
+    }
+};
